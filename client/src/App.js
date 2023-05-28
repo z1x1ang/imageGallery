28d4c9cf050a702dccf2b94a2eafc9263c8d5cd5
tian zixiang <sixheart624@gmail.com>
2023-05-28 10:15:30 +0800

    updated

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import {getImages} from './api'
+import './App.css'
+const App=()=>{
+  const [imageList,setImageList]=useState([]);
+  const [nextCursor,setNextCursor]=useState(null);
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const responseJson=await getImages();
+      setImageList(responseJson.resources);
+      setNextCursor(responseJson.next_cursor);
+    }
+    fetchData();
+  },[]);
+  const handleLoadMoreButtonClick=async()=>{
+    const responseJson=await getImages(nextCursor);
+    setImageList((currentImageList)=>[
+      ...currentImageList,
+      ...responseJson.resources,
+    ]);
+    setNextCursor(responseJson.next_cursor)
+
+  }
+   return (
+    <>
+   <div className="image-grid"> 
+   {
+   imageList.map((image)=>(<img src={image.url} alt={image.public_id}></img>
+   ))}
+    </div>
+    <div className="footer">
+      {nextCursor && <button onClick={handleLoadMoreButtonClick}>Load More</button>}
+    </div>
+    </>
+);
+};
+export default App;
---
+++ client/src/api.js
@@ -0,0 +1,14 @@
+const API_URL=process.env.REACT_APP_API_URL
+
+const getImages=async(nextCursor)=>{
+    const params=new URLSearchParams();
+    if(nextCursor){
+        params.append('next_cursor',nextCursor);
+    }
+    const response=await fetch(`${API_URL}/photos?${params}`);
+   
+    const responseJson=await response.json();
+  
+    return responseJson;
+}
+export {getImages}

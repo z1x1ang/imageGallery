3aec470496468e3f0271faccc3fc164db7967298
z1x1ang <2360283295@qq.com>
2023-05-28 13:32:58 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ import './App.css'
 const App=()=>{
   const [imageList,setImageList]=useState([]);
   const [nextCursor,setNextCursor]=useState(null);
-
+  const [loading, setLoading] = useState(true);
   useEffect(()=>{
     const fetchData=async()=>{
       const responseJson=await getImages();
       setImageList(responseJson.resources);
       setNextCursor(responseJson.next_cursor);
+      setLoading(false);
     }
     fetchData();
   },[]);
@@ -18,13 +19,14 @@ const App=()=>{
     setImageList((currentImageList)=>[
       ...currentImageList,
       ...responseJson.resources,
+      onLoad={() => setLoading(false)}
     ]);
     setNextCursor(responseJson.next_cursor)
 
   }
    return (
     <>
-   <h1 id="loading-text">loading……</h1>
+   {loading&&<h1 id="loading-text">loading……</h1>}
    <div className="image-grid"> 
    {
    imageList.map((image)=>(<img src={image.url} alt={image.public_id} onload="document.getElementById('loading-text').style.display = 'none';"></img>
